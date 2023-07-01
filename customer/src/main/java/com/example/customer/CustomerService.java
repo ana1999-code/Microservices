12f4e59072b6e44caad7d6dda473e815cc5ef080
Ana <anavlas1999@gmail.com>
2023-07-01 19:27:02 +0300

    Add clients module using Open Feign

--- customer/src/main/java/com/example/customer/CustomerService.java
+++ customer/src/main/java/com/example/customer/CustomerService.java
@@ -1,5 +1,7 @@
 package com.example.customer;
 
+import com.example.clients.fraud.FraudCheckResponse;
+import com.example.clients.fraud.FraudClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +16,8 @@ public class CustomerService {
 
     private final RestTemplate restTemplate;
 
+    private final FraudClient fraudClient;
+
     public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
         Customer customer = Customer.builder()
                 .firstName(customerRegistrationRequest.firstName())
@@ -22,11 +26,7 @@ public class CustomerService {
                 .build();
         customerRepository.saveAndFlush(customer);
 
-        var fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-        );
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
 
         if (Objects.requireNonNull(fraudCheckResponse).isFraudster()){
             throw new IllegalStateException("fraudster");
